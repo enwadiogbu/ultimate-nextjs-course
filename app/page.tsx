4985a380d9808fad46ef2aa55017b8c50e4034f8
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-11-03 18:50:10 -0700

    added all the assets for the project and description for the metadata within the metadata export function in the layout.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 const Home = () => {
-  <>
-    <h1 className="h1-bold font-inter text-purple-500">
-      Welcome to the world of Next.JS
-    </h1>
-    ;
-    {/* <h1 className="h1-bold font-space-grotesk">
-      Welcome to the world of Next.JS(Inter)
-    </h1>
-    ; */}
-  </>;
+  return (
+    <>
+      <h1 className="h1-bold font-inter text-purple-500">
+        Welcome to the world of Next.JS
+      </h1>
+      <h1 className="h1-bold font-inter text-purple-500">
+        Welcome to the world of Next.JS(Inter)
+      </h1>
+    </>
+  );
 };
 
 export default Home;
